587355cf94a5032150015afbb737861bfa5b046d
phanisai Bhogadi <phanisaibhogadi@gmail.com>
2025-01-23 15:18:53 +0530

    Update webasg.tf

--- webasg.tf
+++ webasg.tf
@@ -16,14 +16,14 @@ resource "aws_launch_template" "swiggy-web-template" {
   name_prefix   = "swiggy-web-template"
   image_id      = "ami-0ed194d7eff6d2f81"
   instance_type = "t2.micro"
-  key_name      = "devopsbyraham"
+  key_name      = "mango"
   network_interfaces {
     associate_public_ip_address = true
     security_groups             = [aws_security_group.swiggy-ec2-asg-sg.id]
   }
   user_data = base64encode(file("apache.sh"))
   lifecycle {
-    prevent_destroy = false
+    prevent_destroy = true
     ignore_changes  = all
   }
 }
